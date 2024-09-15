2808f58b5a8934d136c3c10519a781e55c5185e6
saeed mohammdiha <saeed1440124@gmail.com>
2024-09-15 08:20:17 +0330

    refactor: change the name and the types of the store

---
+++ src/business/stores/issueCheck/useIssueCheckWizardData.ts
@@ -0,0 +1,47 @@
+import { CheckSheet } from 'common/entities/cheque/Digital Cheque/GetChecksheets/GetChecksheetsResponse';
+import { RecieverRequest } from 'common/entities/cheque/Digital Cheque/IssueChequeInitiate/IssueChequeInitiateRequest';
+import { IssueChequeOverView } from 'common/entities/cheque/Digital Cheque/Verify Otp/VerifyOtpResponse';
+import { create } from 'zustand';
+
+interface Functions {
+	setNewDataToWizard: <T extends keyof IssuCheckWizardData>(data: Record<T, IssuCheckWizardData[T]>) => void;
+	reset: () => void;
+}
+
+interface IssuCheckWizardData {
+	selectCheckPage?: {
+		selectedAccount: string;
+		selectedCheckbook: string;
+		selectedSheet: string;
+		checkData: CheckSheet;
+	};
+	checkInfoPage?: {
+		checkAmount: string;
+		date: Date;
+		reason: { value: string; name: string };
+		description: string;
+	};
+	addReceiverPage?: {
+		receivers?: RecieverRequest[];
+		signitureRequirementData?: {
+			issueChequeKey?: string;
+			isSingleSignatureLegal?: boolean;
+		};
+	};
+	otpPage?: {
+		needInquiryWithDrawalGroup?: boolean;
+		issueChequeOverView?: IssueChequeOverView;
+	};
+}
+
+export const useIssueCheckWizardData = create<IssuCheckWizardData & Functions>((set) => ({
+	setNewDataToWizard: (data) =>
+		set((state) => ({
+			...state,
+			...data
+		})),
+
+	reset: () => {
+		set(() => ({}));
+	}
+}));
