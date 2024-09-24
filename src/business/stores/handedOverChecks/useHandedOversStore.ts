8ad7ad791267849327063cf59b5071ee555c5311
saeed mohammdiha <saeed1440124@gmail.com>
2024-09-24 12:37:31 +0330

    feat: add the store for handed over checks

---
+++ src/business/stores/handedOverChecks/useHandedOversStore.ts
@@ -0,0 +1,26 @@
+import { ChakavakGetEChequeListResponse, chakavakCheck } from 'common/entities/cheque/chekList/ChakavakGetEChequeList/ChakavakGetEChequeListResponse';
+import { GetAllRelatedCustomersResponse } from 'common/entities/cheque/chekList/GetAllRelatedCustomers/GetAllRelatedCustomersResponse';
+import { create } from 'zustand';
+
+interface Actions {
+	addNewStoreData: <T extends keyof HandedOversData>(data: Record<T, HandedOversData[T]>) => void;
+	resetStore: () => void;
+}
+
+interface HandedOversData {
+	selectListPage?: { relatedCustomers: GetAllRelatedCustomersResponse };
+	detailsPage?: { check: chakavakCheck };
+	listPage?: { allCheckList: ChakavakGetEChequeListResponse };
+}
+
+export const useHandedOversData = create<Actions & HandedOversData>((set) => ({
+	addNewStoreData: (data) => {
+		set((store) => ({
+			...store,
+			...data
+		}));
+	},
+	resetStore: () => {
+		set((store) => ({}));
+	}
+}));
