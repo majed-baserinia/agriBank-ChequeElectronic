5e79e9d231d7b9849f4116c6d33a931f0a3e716f
saeed mohammdiha <saeed1440124@gmail.com>
2024-09-24 12:36:48 +0330

    refactor: move the onerror fuction in to the api call hook

--- src/business/hooks/cheque/cashCheque/useChakavakPutECheque.ts
+++ src/business/hooks/cheque/cashCheque/useChakavakPutECheque.ts
@@ -1,24 +1,37 @@
 import { Mediator } from '@Mediatr/index';
 import { useMutation } from '@tanstack/react-query';
 import ChakavakPutEChequeCommand from 'business/application/cheque/cashCheck/ChakavakPutECheque/ChakavakPutEChequeCommand';
+import { pushAlert } from 'business/stores/AppAlertsStore';
 import { ErrorType } from 'common/entities/ErrorType';
 import { ChakavakPutEChequeRequest } from 'common/entities/cheque/cashCheck/ChakavakPutECheque/ChakavakPutEChequeRequest';
 import { ChakavakPutEChequeResponse } from 'common/entities/cheque/cashCheck/ChakavakPutECheque/ChakavakPutEChequeResponse';
+import { useNavigate } from 'react-router-dom';
+import { paths } from 'ui/route-config/paths';
 
 const mediator = new Mediator();
 
 export default function useChakavakPutECheque() {
+	const navigate = useNavigate()
+
 	return useMutation<ChakavakPutEChequeResponse, ErrorType<ChakavakPutEChequeRequest>, ChakavakPutEChequeCommand>({
 		mutationFn: (data: ChakavakPutEChequeCommand) =>
 			mediator.send<ChakavakPutEChequeResponse>(new ChakavakPutEChequeCommand(data)),
 		onMutate: (variables) => {
 			return () => variables;
 		},
-		onSuccess: (data) => {
-			return () => data;
+		onSuccess: (res) => {
+			pushAlert({
+				type: 'success',
+				messageText: res.message,
+				hasConfirmAction: true,
+				actions: {
+					onCloseModal: () => navigate(paths.Home),
+					onConfirm: () => navigate(paths.Home)
+				}
+			});
 		},
 		onError: (error, variables) => {
-			return () => variables;
+			pushAlert({ type: 'error', messageText: error.detail, hasConfirmAction: true });
 		}
 	});
 }
--- src/business/hooks/cheque/checklist/useGetAllRelatedCustomers.ts
+++ src/business/hooks/cheque/checklist/useGetAllRelatedCustomers.ts
@@ -1,17 +1,37 @@
 import { Mediator } from '@Mediatr/index';
 import { useQuery } from '@tanstack/react-query';
 import GetAllRelatedCustomersQuery from 'business/application/cheque/checkList/GetAllRelatedCustomers/GetAllRelatedCustomersQuery';
+import { pushAlert } from 'business/stores/AppAlertsStore';
 import { ErrorType } from 'common/entities/ErrorType';
 import { GetAllRelatedCustomersResponse } from 'common/entities/cheque/chekList/GetAllRelatedCustomers/GetAllRelatedCustomersResponse';
+import { useNavigate } from 'react-router-dom';
+import { paths } from 'ui/route-config/paths';
 
 const mediator = new Mediator();
 
-const useGetAllRelatedCustomers = (serviceName: string) =>
-	useQuery<GetAllRelatedCustomersResponse, ErrorType<{}>, GetAllRelatedCustomersResponse>({
+const useGetAllRelatedCustomers = (serviceName: string) => {
+	const navigate = useNavigate()
+	return useQuery<GetAllRelatedCustomersResponse, ErrorType<{}>, GetAllRelatedCustomersResponse>({
 		queryKey: ['GetAllRelatedCustomers'],
 		queryFn: () => mediator.send<GetAllRelatedCustomersResponse>(new GetAllRelatedCustomersQuery(serviceName)),
 		staleTime: 0, //ms('30m')
-		retry: false
+		retry: false,
+		onError: (err) => {
+			pushAlert({
+				type: 'error',
+				messageText: err.detail,
+				hasConfirmAction: true,
+				actions: {
+					onCloseModal: () => {
+						navigate(paths.Home);
+					},
+					onConfirm: () => {
+						navigate(paths.Home);
+					}
+				}
+			});
+		}
 	});
+};
 
 export default useGetAllRelatedCustomers;
