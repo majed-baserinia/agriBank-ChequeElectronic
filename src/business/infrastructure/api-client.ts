5cadc79320a5d98187d45705ba5149560e4fe343
m-setayesh-m <m-setayesh-m@agri-bank.com>
2025-01-20 12:33:55 +0330

    fix: show the first error thrown caused by unauthorized request not the second one

--- src/business/infrastructure/api-client.ts
+++ src/business/infrastructure/api-client.ts
@@ -1,23 +1,23 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
-import axiosRetry from 'axios-retry';
-import { sendPostmessage } from 'business/hooks/postMessage/useInitPostMessage';
-import { pushAlert } from 'business/stores/AppAlertsStore';
-import ApiConfigSingleton from 'business/stores/api-config-singleton';
-import useInitialSettingStore from 'business/stores/initial-setting-store';
-import { ErrorType } from 'common/entities/ErrorType';
-import i18n from 'i18n';
-import { t } from 'i18next';
-import { clearAuth, getAuthTokens, saveAuthTokens } from './auth-service';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import axiosRetry from "axios-retry";
+import { sendPostmessage } from "business/hooks/postMessage/useInitPostMessage";
+import { pushAlert } from "business/stores/AppAlertsStore";
+import ApiConfigSingleton from "business/stores/api-config-singleton";
+import useInitialSettingStore from "business/stores/initial-setting-store";
+import { ErrorType } from "common/entities/ErrorType";
+import i18n from "i18n";
+import { t } from "i18next";
+import { clearAuth, getAuthTokens, saveAuthTokens } from "./auth-service";
 
 const axiosInstance = axios.create({
 	headers: {
-		'Content-Type': 'application/json'
+		"Content-Type": "application/json"
 	}
 });
 
 const axiosForLogin = axios.create({
 	headers: {
-		'Content-Type': 'application/json'
+		"Content-Type": "application/json"
 	}
 });
 
@@ -31,10 +31,13 @@ axiosRetry(axiosInstance, {
 const refreshToken = async (refreshToken: string): Promise<string | undefined> => {
 	const authTokens = getAuthTokens();
 	const baseUrl = ApiConfigSingleton.getApiConfig().baseUrl;
-	axiosForLogin.defaults.headers.common['Authorization'] = `Bearer ${authTokens?.idToken}`;
-	const response = await axiosForLogin.post<{ idToken: string; refreshToken: string }>(baseUrl + '/refreshtoken', {
-		refreshToken: refreshToken
-	});
+	axiosForLogin.defaults.headers.common["Authorization"] = `Bearer ${authTokens?.idToken}`;
+	const response = await axiosForLogin.post<{ idToken: string; refreshToken: string }>(
+		baseUrl + "/refreshtoken",
+		{
+			refreshToken: refreshToken
+		}
+	);
 	const newIdToken = response.data.idToken;
 	const newRefreshToken = response.data.refreshToken;
 	saveAuthTokens({ idToken: newIdToken, refreshToken: newRefreshToken });
@@ -42,8 +45,8 @@ const refreshToken = async (refreshToken: string): Promise<string | undefined> =
 };
 
 axiosForLogin.interceptors.request.use((config) => {
-	config.headers['accept-language'] = i18n.language;
-	config.headers['os-type'] = useInitialSettingStore.getState().settings.osType;
+	config.headers["accept-language"] = i18n.language;
+	config.headers["os-type"] = useInitialSettingStore.getState().settings.osType;
 
 	return config;
 });
@@ -54,14 +57,17 @@ axiosInstance.interceptors.request.use((config) => {
 		const { idToken } = authTokens;
 		config.headers.Authorization = `Bearer ${idToken}`;
 	}
-	config.headers['accept-language'] = i18n.language;
-	config.headers['os-type'] = useInitialSettingStore.getState().settings.osType;
+	config.headers["accept-language"] = i18n.language;
+	config.headers["os-type"] = useInitialSettingStore.getState().settings.osType;
 	return config;
 });
 
 axiosInstance.interceptors.response.use(
 	(response: AxiosResponse) => response,
 	async <TResponse>(error: AxiosError) => {
+		if (!error.config) {
+			return Promise.reject(error);
+		}
 		const originalRequest = error.config;
 
 		const authTokens = getAuthTokens();
@@ -70,21 +76,21 @@ axiosInstance.interceptors.response.use(
 			const refreshTokenValue = authTokens.refreshToken;
 			try {
 				const newIdToken = await refreshToken(refreshTokenValue!);
-				axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newIdToken}`;
+				axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${newIdToken}`;
 				return axiosInstance.request(originalRequest!);
 			} catch (_) {
 				clearAuth();
 				window.location.href = import.meta.env.BASE_URL;
-				sendPostmessage('tokenIsNotValid', 'true');
+				sendPostmessage("tokenIsNotValid", "true");
 			}
 		} else if (error.response?.status == 400 && error?.response?.data) {
 			throw prepareErrorType(<ErrorType<TResponse>>error?.response?.data);
 		}
 
-		if (error.message === 'Network Error' && error.response?.status !== 500) {
+		if (error.message === "Network Error" && error.response?.status !== 500) {
 			pushAlert({
-				type: 'error',
-				messageText: t('netErr'),
+				type: "error",
+				messageText: t("netErr"),
 				hasConfirmAction: true
 			});
 		}
@@ -104,7 +110,7 @@ class APIClient<TBody, TResponse> {
 	};
 
 	get = (id: number | string) => {
-		return axiosInstance.get<TResponse>(this.endpoint + '/' + id).then((res) => res.data);
+		return axiosInstance.get<TResponse>(this.endpoint + "/" + id).then((res) => res.data);
 	};
 
 	post = (body: TBody) => {
@@ -117,18 +123,18 @@ class APIClient<TBody, TResponse> {
 			.then((res) => res.data)
 			.catch((error: AxiosError) => {
 				if (error.response?.status == 400) {
-					throw new Error((<ErrorType<object>>error?.response?.data).detail || 'Internal Error');
+					throw new Error((<ErrorType<object>>error?.response?.data).detail || "Internal Error");
 				}
-				throw new Error('Internal Error');
+				throw new Error("Internal Error");
 			});
 	};
 }
 
 function prepareErrorType<T>(error: ErrorType<T>) {
 	if (!error.detail || error.detail.trim().length === 0) {
-		error.detail = i18n.t('Internal Error');
+		error.detail = i18n.t("Internal Error");
 	}
-	error.instance = error?.instance || '';
+	error.instance = error?.instance || "";
 	return error;
 }
 
