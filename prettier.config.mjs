11e35d342a5865cbcad63632145faf8b61c3bd35
m-setayesh-m <m-setayesh-m@agri-bank.com>
2025-01-20 09:40:40 +0330

    chore: updated prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+import { agribankPrettierConfig } from "@agribank/prettier-config";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+	...agribankPrettierConfig
+};
+
+export default config;
